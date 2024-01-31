5f782ceef35c8dc4982e630176b482df5dece416
Ntsikayomzi Phenyo Ngcakani <85479736+TsikaBullRat@users.noreply.github.com>
2024-01-31 17:04:23 +0200

    Update script.js

--- res/script.js
+++ res/script.js
@@ -6,7 +6,7 @@ const settings = {
 	url: 'https://spotify23.p.rapidapi.com/albums/?ids=262o5a05pMUDSV2uzo5EwQ',
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '4298e3da2emshf39b08d3bbe2f3cp1ff22ajsn4b60a977651a',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
 };
